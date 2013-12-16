f15cec6ed03de929d3db0c163c1dac1fa142a1fe
Simeon Mugisha <cmeon@users.noreply.github.com>
2013-12-17 06:45:09 +0800

    Update install-arangodb.sh

--- install-arangodb.sh
+++ install-arangodb.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-sudo su
-
 wget http://www.arangodb.org/repositories/arangodb/xUbuntu_12.04/Release.key
-apt-key add - < Release.key
+sudo apt-key add - < Release.key
 
 RELEASE_VERSION=$(lsb_release -r | sed -e 's/Release:[\t]*//')
-echo "deb http://www.arangodb.org/repositories/arangodb/xUbuntu_${RELEASE_VERSION}/ /" >> /etc/apt/sources.list.d/arangodb.list
-apt-get -qq update
-apt-get -y install arangodb=1.4.3
+sudo echo "deb http://www.arangodb.org/repositories/arangodb/xUbuntu_${RELEASE_VERSION}/ /" >> /etc/apt/sources.list.d/arangodb.list
+sudo apt-get -qq update
+sudo apt-get -y install arangodb=1.4.3
 
 exit
