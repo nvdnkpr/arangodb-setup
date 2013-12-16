550e42fe0da765eb630542090cbc500643be8dce
cmeon <cmeo226@yahoo.com>
2013-12-17 02:58:51 +0800

    Install arangodb shell script

---
+++ install-arangodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget http://www.arangodb.org/repositories/arangodb/xUbuntu_12.04/Release.key
+apt-key add - < Release.key
+
+RELEASE_VERSION=$(lsb_release -r | sed -e 's/Release:[\t]*//')
+echo "deb http://www.arangodb.org/repositories/arangodb/xUbuntu_${RELEASE_VERSION}/ /" >> /etc/apt/sources.list.d/arangodb.list
+apt-get -qq update
+apt-get install arangodb=1.4.3
